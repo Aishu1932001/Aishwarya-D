0118e446909025c532f1dacbd9cb63b12e1c1d19
Aishu1932001 <110168081+Aishu1932001@users.noreply.github.com>
2022-07-28 16:40:11 +0530

    Add files via upload

--- Author Test.java
+++ Author Test.java
@@ -3,7 +3,19 @@ package com.oops.demo;
 public class Author Test {
     
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Author a1=new Author ("Aishwarya","cartoon");
+        Author a2=new Author("Ranjith","jackson");
+        
+        system.out.println(a1.getFirstName());
+        system.out.println(a2.getLastName());
+        
+        system.out.println(a2.tosyring());
+        
+        Author a3=new Author();
+        Author a4;
+        //system.out.println(a4);
+        a4=a2;
+        system.out.println(a4);
     }
     
 }
