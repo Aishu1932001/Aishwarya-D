f280f81bb2ad69e7cba5baa27d65bb49c8daf6fd
Aishu1932001 <110168081+Aishu1932001@users.noreply.github.com>
2022-07-28 16:22:54 +0530

    Add files via upload

---
+++ Author Test.java
@@ -0,0 +1,9 @@
+package com.oops.demo;
+
+public class Author Test {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}
---
+++ Main.java
@@ -0,0 +1,34 @@
+package com.oops.demo;
+public class Author{
+    private string FirstName;
+    private string LastName;
+    
+    public Author(){
+        
+    }
+    
+public author(string FirstName,string LastName){
+    this.FirstName=string FirstName;
+    this.LastName=string LastName;
+}
+    
+public void setFirstName( string FirstName ){
+    this.FirstName=FirstName;
+    }
+    
+public void setFirstname( string lastname)
+    this.Lastname=Lastname;
+}
+	public string getFirstname( ){
+    return"FirstName":
+}
+
+public string getlastname(){
+return"LastName":
+}
+
+@override
+public string to string(){
+    return "Author[" + FirstName + " , " + LastName + "]";
+}
+}
